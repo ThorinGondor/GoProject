b0b4f028a2b2a91a96631621341c2b4cc7f33d1a
LiXinze <1360002051@qq.com>
2019-03-28 22:01:06 +0800

    Initial commit

---
+++ M0_HelloGo/AlphaGo.go
@@ -0,0 +1,7 @@
+package M0_HelloGo
+
+import "fmt"
+
+func main()  {
+	fmt.Print("Hello AlphaGo!")
+}
---
+++ M0_HelloGo/HelloGo.go
@@ -0,0 +1,12 @@
+package M0_HelloGo
+
+import "fmt"
+
+func main(){
+	evangelion("碇真嗣")
+}
+
+
+func evangelion(fullname string){
+	fmt.Println("Hello, " + fullname)
+}
